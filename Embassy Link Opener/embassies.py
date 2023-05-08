9d6dc77ea228b6c01e71eefbed1f55224ac748b4
Celinova <128658483+Celinova@users.noreply.github.com>
2023-05-08 08:29:36 -0400

    Update embassies.py
    Now uses nsdotpy and doesn't have to open a link.

--- Embassy Link Opener/embassies.py
+++ Embassy Link Opener/embassies.py
@@ -1,10 +1,34 @@
-import webbrowser
-import time
-import requests
 import xml.etree.ElementTree as ET
 
-def get_all_regions(headers):
+import requests
+from nsdotpy.session import NSSession
+
+
+def login(session, nation: str, password: str) -> bool:
+    return session.login(nation, password)
+
+
+session = NSSession(
+    "Destiny's Embassy Collecting Script",
+    "0.9",
+    "The Chinese Soviet",
+    "The Chinese Soviet",
+    'enter',
+    link_to_src='tbd',
+    logger=None
+)
+
+authenticated = login(session, "name", "password")
+
+if authenticated:
+    print("Authentication successful!")
+else:
+    print("Authentication failed.")
+
+
+def get_all_regions():
     url = "https://www.nationstates.net/cgi-bin/api.cgi?q=regions"
+    headers = {"User-Agent": "Useragent"}
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -15,6 +39,7 @@ def get_all_regions(headers):
         print("Failed to fetch regions. API response status:", response.status_code)
         return []
 
+
 def load_processed_regions(file_name):
     try:
         with open(file_name, "r") as f:
@@ -23,34 +48,32 @@ def load_processed_regions(file_name):
     except FileNotFoundError:
         return set()
 
+
 def save_processed_region(file_name, region):
     with open(file_name, "a") as f:
         f.write(region + ",")
 
-def open_region_links(regions, batch_size, processed_regions_file):
-    base_url = "https://www.nationstates.net/region="
 
-    for i in range(0, len(regions), batch_size):
-        for region in regions[i:i + batch_size]:
-            if region not in processed_regions:
-                url = base_url + region
-                webbrowser.open(url)
-                save_processed_region(processed_regions_file, region)
-        print(f"Opened regions {i + 1} to {min(i + batch_size, len(regions))}. Press enter to continue with the next batch.")
+def send_embassy_requests(regions, session, processed_regions_file):
+    for i, region in enumerate(regions):
+        if not session.request_embassy(region):
+            print(f"Failed to send embassy request to {region}.")
+        else:
+            print(f"Successfully sent embassy request to {region}.")
+            save_processed_region(processed_regions_file, region)
+        print(f"Processed region {i + 1} of {len(regions)}. Press space to continue with the next region.")
         input()
 
-if __name__ == "__main__":
-    ns_name = input("Enter your NationStates nation (please use underscores if there are spaces): ")
-    headers = {"User-Agent": f"{ns_name}/Link Opener for NS embassy collecting (https://github.com/Celinova/Embassy-Collecting-Scripts/edit/main/Embassy%20Link%20Opener/embassies.py)"}
 
-    regions = get_all_regions(headers)
-    batch_size = int(input("Enter the number of regions to process at a time: "))
+if __name__ == "__main__":
+    regions = get_all_regions()
     processed_regions_file = "processed_regions.txt"
 
     processed_regions = load_processed_regions(processed_regions_file)
     remaining_regions = [region for region in regions if region not in processed_regions]
 
     if remaining_regions:
-        open_region_links(remaining_regions, batch_size, processed_regions_file)
+        remaining_regions_reversed = remaining_regions[::-1]
+        send_embassy_requests(remaining_regions_reversed, session, processed_regions_file)
     else:
         print("All regions have been processed.")
